2f14017200d1f1668f069807ddbc3a0ff4623ab5
jackhhh <mj@etengpc.com>
2017-09-13 20:05:39 +0800

    change mysql to mysqli

--- connect.php
+++ connect.php
@@ -5,9 +5,8 @@ $db_pass="dbpass";
 $db_name="dbname";
 $timezone="Asia/Shanghai";
 
-$link=mysql_connect($host,$db_user,$db_pass);
-mysql_select_db($db_name,$link);
-mysql_query("SET names UTF8");
+$link=mysqli_connect($host, $db_user, $db_pass, $db_name);
+mysqli_query("SET names UTF8");
 
 header("Content-Type: text/html; charset=utf-8");
 date_default_timezone_set($timezone); //北京时间
--- json.php
+++ json.php
@@ -2,11 +2,11 @@
 include_once("connect.php");
 
 $sql = "select active from mapdata order by id asc";
-$query = mysql_query($sql);
+$query = mysqli_query($sql);
 
-while($row=mysql_fetch_array($query)){
-	$arr[] = $row['active'];
+while ($row=mysqli_fetch_array($query)) {
+    $arr[] = $row['active'];
 }
 echo json_encode($arr);
-mysql_close($link);
+mysqli_close($link);
 ?>
