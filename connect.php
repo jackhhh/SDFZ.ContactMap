9d296569f6aa3a5c88b1d2a7cd445679c41e9abd
Jack <me@jackh.cn>
2017-10-25 15:46:46 +0800

    Fix mysqli parameters bug.

--- connect.php
+++ connect.php
@@ -6,7 +6,7 @@ $db_name="dbname";
 $timezone="Asia/Shanghai";
 
 $link=mysqli_connect($host, $db_user, $db_pass, $db_name);
-mysqli_query("SET names UTF8");
+mysqli_query($link, "SET names UTF8");
 
 header("Content-Type: text/html; charset=utf-8");
 date_default_timezone_set($timezone); //北京时间
